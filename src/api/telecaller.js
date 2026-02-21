4316d03cfd6841e51ebf9ca8c90562bb0a2907d0
ThenamXe0n <thenameet0@gmail.com>
2026-02-21 18:20:32 +0530

    chore: update API base URL to ngrok for remote access

--- src/api/telecaller.js
+++ src/api/telecaller.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://zora-undedicated-janean.ngrok-free.dev/api',
   withCredentials: true,
   headers: { 'Content-Type': 'application/json', "ngrok-skip-browser-warning": "true" },
 });
